bcede4ed7a1d01434a4c604e770965ab407b7e38
Mark Williams <mark@thehappyhome.co>
2016-01-25 12:25:00 -0800

    xhr_streaming actually works; add streaming.txt to placate the sockjs-client tests

--- acceptance/sockjs_protocol_test_server.py
+++ acceptance/sockjs_protocol_test_server.py
@@ -1,3 +1,4 @@
+from twisted.internet import reactor, task
 from twisted.internet.protocol import Factory, Protocol
 
 from txdarn.application import TxDarn
@@ -26,7 +27,6 @@ class AmplifyProtocol(Protocol):
     def dataReceived(self, data):
         response = ["x" * 2 ** self._parseNumber(datum) for datum in data]
         self.transport.write(response)
-
 
 options = {
     'sockjs-url': 'https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.0.3/sockjs.js',
@@ -59,9 +59,22 @@ def close(request):
 def noWS(request):
     return darnitNoWS.app.resource()
 
+
 @app.route('/amplify', strict_slashes=False, branch=True)
 def amplify(request):
     return darnitAmplify.app.resource()
+
+
+@app.route('/streaming.txt', strict_slashes=False, branch=True)
+def streaming_txt(request):
+    request.setHeader(b'Content-Type', b'text/plain')
+    request.setHeader(b'Access-Control-Allow-Origin', b'*')
+    request.write(b'a' * 2048 + b'\n')
+
+    def done():
+        request.write(b'b\n')
+
+    return task.deferLater(reactor, .25, done)
 
 
 app.run('localhost', 8081)
