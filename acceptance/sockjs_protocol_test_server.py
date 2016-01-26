0f9f13581ab83866dc37746d5d9f3ae29167ea5e
Mark Williams <mark@thehappyhome.co>
2016-01-25 21:01:04 -0800

    remove klein

--- acceptance/sockjs_protocol_test_server.py
+++ acceptance/sockjs_protocol_test_server.py
@@ -1,9 +1,10 @@
-from twisted.internet import reactor, task
+from twisted.internet import reactor
+from twisted.web.resource import Resource
+from twisted.web.server import Site, NOT_DONE_YET
+from twisted.internet import endpoints
 from twisted.internet.protocol import Factory, Protocol
 
-from txdarn.application import TxDarn
-
-import klein
+from txdarn.resources import TxDarn
 
 
 class EchoProtocol(Protocol):
@@ -29,52 +30,55 @@ class AmplifyProtocol(Protocol):
         self.transport.write(response)
 
 options = {
-    'sockjs-url': 'https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.0.3/sockjs.js',
-    'websockets': True,
+    'sockJSURL': 'https://cdnjs.cloudflare.com/ajax'
+    '/libs/sockjs-client/1.0.3/sockjs.js',
+    'websocketsEnabled': True,
     'timeout': 5.0,
     'maximumBytes': 4096}
-darnit = TxDarn(Factory.forProtocol(EchoProtocol), options)
-darnitClose = TxDarn(Factory.forProtocol(CloseProtocol), options)
+
+darnit = TxDarn(Factory.forProtocol(EchoProtocol), **options)
+darnitClose = TxDarn(Factory.forProtocol(CloseProtocol), **options)
 
 noWS = options.copy()
-noWS['websockets'] = False
-darnitNoWS = TxDarn(Factory.forProtocol(EchoProtocol), noWS)
+noWS['websocketsEnabled'] = False
+darnitNoWS = TxDarn(Factory.forProtocol(EchoProtocol), **noWS)
 
-darnitAmplify = TxDarn(Factory.forProtocol(AmplifyProtocol), options)
-
-app = klein.Klein()
+darnitAmplify = TxDarn(Factory.forProtocol(AmplifyProtocol), **options)
 
 
-@app.route('/echo/', strict_slashes=False, branch=True)
-def root(request):
-    return darnit.app.resource()
+class StreamingResource(Resource):
+
+    def render_GET(self, request):
+        request.setHeader(b'Content-Type', b'text/plain')
+        request.setHeader(b'Access-Control-Allow-Origin', b'*')
+        request.write(b'a' * 2048 + b'\n')
+
+        def done():
+            request.write(b'b\n')
+            request.finish()
+
+        reactor.callLater(0.25, done)
+        return NOT_DONE_YET
 
 
-@app.route('/close/', strict_slashes=False, branch=True)
-def close(request):
-    return darnitClose.app.resource()
+testServer = Resource()
+testServer.putChild(b'echo', darnit)
+testServer.putChild(b'close', darnitClose)
+testServer.putChild(b'disabled_websocket_echo', darnitNoWS)
+testServer.putChild(b'amplify', darnitAmplify)
+testServer.putChild(b'streaming.txt', StreamingResource())
 
 
-@app.route('/disabled_websocket_echo/', strict_slashes=False, branch=True)
-def noWS(request):
-    return darnitNoWS.app.resource()
+def runWithoutLog():
+    site = Site(testServer, logPath=b'/tmp/log')
+    endpoints.serverFromString(reactor, 'tcp:8081').listen(site)
+    reactor.run()
 
 
-@app.route('/amplify', strict_slashes=False, branch=True)
-def amplify(request):
-    return darnitAmplify.app.resource()
+def runWithLog():
+    import sys
+    from twisted.python import log
+    log.startLogging(sys.stdout)
+    runWithoutLog()
 
-
-@app.route('/streaming.txt', strict_slashes=False, branch=True)
-def streaming_txt(request):
-    request.setHeader(b'Content-Type', b'text/plain')
-    request.setHeader(b'Access-Control-Allow-Origin', b'*')
-    request.write(b'a' * 2048 + b'\n')
-
-    def done():
-        request.write(b'b\n')
-
-    return task.deferLater(reactor, .25, done)
-
-
-app.run('localhost', 8081)
+runWithLog()
